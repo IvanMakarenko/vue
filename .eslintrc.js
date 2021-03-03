3947a7d3eba50656483669e857205e91add3f9b2
Makarenko_i_v <shketiam@bigmir.net>
2021-03-03 23:12:25 +0200

    Update node, take new packages, eslinter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "plugins": [
+        "vue",
+    ],
+    "rules": {
+    }
+};
